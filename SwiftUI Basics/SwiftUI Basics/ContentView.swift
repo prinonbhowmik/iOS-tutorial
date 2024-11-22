24710eea9f52e9df3bd4a5771dc322ca9d41add2
prinonbhowmik <prinonarnob@gmail.com>
2024-11-22 17:56:35 +0600

    onTapGesture offset animation from swiftUI basics

--- SwiftUI Basics/SwiftUI Basics/ContentView.swift
+++ SwiftUI Basics/SwiftUI Basics/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let colorList1 :[Color] = [.blue,.gray,.indigo]
+    let colorList2 :[Color] = [.pink,.red,.purple]
+    
+    @State var screenTapped :Bool = false
+    
+    @State private var offSetY :CGFloat = -1000
+    
+    var colorChange:[Color] {
+        screenTapped ? colorList1 : colorList2
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            
+            LinearGradient(colors : colorChange, startPoint: .topLeading, endPoint: .bottomTrailing)
+                .opacity(0.7)
+                .ignoresSafeArea()
+            
+            Text("SwiftUI basics" )
+                .font(.system(size: 26))
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .foregroundStyle(.white)
+                .padding()
+                .offset(y : offSetY)
+                .rotation3DEffect(
+                    .degrees(screenTapped ? 0 : 360),
+                    axis: (x : 1, y : 1, z : 1))
         }
-        .padding()
+        .onTapGesture {
+            screenTapped.toggle()
+            withAnimation(.easeIn(duration: 0.3)){
+                offSetY = screenTapped ? 0 : -1000
+            }
+            
+        }
+            
     }
 }
 

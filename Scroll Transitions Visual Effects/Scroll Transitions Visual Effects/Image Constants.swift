d50548e6beb15a10010e1ffa63bdb767a2b9c8ed
prinonbhowmik <prinonarnob@gmail.com>
2024-11-30 02:49:33 +0600

    Scroll transition

---
+++ Scroll Transitions Visual Effects/Scroll Transitions Visual Effects/Image Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Image Constants.swift
+//  Scroll Transitions Visual Effects
+//
+//  Created by Prinon bhowmik on 11/30/24.
+//
+
+import Foundation
---
+++ Scroll Transitions Visual Effects/Scroll Transitions Visual Effects/MyImageView.swift
@@ -0,0 +1,18 @@
+//
+//  MyImageView.swift
+//  Scroll Transitions Visual Effects
+//
+//  Created by Prinon bhowmik on 11/30/24.
+//
+
+import SwiftUI
+
+struct MyImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyImageView()
+}

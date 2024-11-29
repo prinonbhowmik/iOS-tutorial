ab97e1fcdfe76c5b4e9acb28d52e1f801566e815
prinonbhowmik <prinonarnob@gmail.com>
2024-11-30 01:21:12 +0600

    Screenshot

---
+++ Scroll Transitions Visual Effects/Scroll Transitions Visual Effects/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Scroll Transitions Visual Effects
+//
+//  Created by Prinon bhowmik on 11/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Scroll Transitions Visual Effects/Scroll Transitions Visual Effects/Scroll_Transitions_Visual_EffectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scroll_Transitions_Visual_EffectsApp.swift
+//  Scroll Transitions Visual Effects
+//
+//  Created by Prinon bhowmik on 11/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Scroll_Transitions_Visual_EffectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
